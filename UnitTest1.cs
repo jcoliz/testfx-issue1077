3ea8fb76274b18ac56c4148d1a287b664af7df4b
James Coliz <jcoliz@outlook.com>
2022-04-22 21:21:23 -0700

    build: dotnet new mstest -o testfx-issue1077

---
+++ UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace testfx_issue1077;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}

5e51c0c8af62d2ac0c25a4b7a22c052574fd42e8
James Coliz <jcoliz@outlook.com>
2022-04-22 21:45:12 -0700

    feat: Successfully reproduced this issue

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -3,18 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+[assembly: Parallelize(Workers = 4, Scope = ExecutionScope.MethodLevel)]
+
 namespace testfx_issue1077;
 
 [TestClass]
 public class UnitTest1
 {
-    public static IEnumerable<object[]> Values => Enumerable.Range(1,10000).Select(x => new object[] { x });
+    public static IEnumerable<object[]> Values => Enumerable.Range(1,100).Select(x => new object[] { x });
 
-    [DynamicData(nameof(Values), DynamicDataSourceType.Property)]
-    [DataTestMethod]
-    public void TestMethod1(int value)
+    private byte[] MakeHash(int value)
     {
-        for(int i = 0; i < value; i++)
-            Console.WriteLine(i);
+        using var md5 = System.Security.Cryptography.MD5.Create();
+        byte[] inputBytes = Enumerable.Repeat<byte>((byte)(value%255),1024).ToArray();        
+        return md5.ComputeHash(inputBytes);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Values), DynamicDataSourceType.Property)]
+    public void Hash1(int value)
+    {
+        var hash = MakeHash(value);
+        Console.WriteLine("{0}", Convert.ToHexString(hash));
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Values), DynamicDataSourceType.Property)]
+    public void Hash2(int value)
+    {
+        var hash = MakeHash(value);
+        Console.WriteLine("{0}", Convert.ToHexString(hash));
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Values), DynamicDataSourceType.Property)]
+    public void Hash3(int value)
+    {
+        var hash = MakeHash(value);
+        Console.WriteLine("{0}", Convert.ToHexString(hash));
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(Values), DynamicDataSourceType.Property)]
+    public void Hash4(int value)
+    {
+        var hash = MakeHash(value);
+        Console.WriteLine("{0}", Convert.ToHexString(hash));
     }
 }

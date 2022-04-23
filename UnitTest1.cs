fa962627eab5723b4949f87775019025589cb345
James Coliz <jcoliz@outlook.com>
2022-04-22 21:27:15 -0700

    feat: Add basic long test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace testfx_issue1077;
@@ -5,8 +8,13 @@ namespace testfx_issue1077;
 [TestClass]
 public class UnitTest1
 {
-    [TestMethod]
-    public void TestMethod1()
+    public static IEnumerable<object[]> Values => Enumerable.Range(1,10000).Select(x => new object[] { x });
+
+    [DynamicData(nameof(Values), DynamicDataSourceType.Property)]
+    [DataTestMethod]
+    public void TestMethod1(int value)
     {
+        for(int i = 0; i < value; i++)
+            Console.WriteLine(i);
     }
 }
